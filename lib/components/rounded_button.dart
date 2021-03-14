60a9d133cc2dc9243381e555c04eae474a04c7c4
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-14 20:23:10 +0000

    Update rounded_button.dart

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -15,19 +15,15 @@ class RoundedButton extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: Material(
-        // color: Colors.blueAccent,
         color: color,
         borderRadius: BorderRadius.circular(30.0),
         elevation: 5.0,
         child: MaterialButton(
           onPressed: onPressed,
-          //Go to registration screen.
-          // Navigator.pushNamed(context, RegistrationScreen.id);
           minWidth: 200.0,
           height: 42.0,
           child: Text(
             btnTitle,
-            // 'Register',
           ),
         ),
       ),
